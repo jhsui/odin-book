60adf79379f32be8bf62f4d3c13f5e1bc0baab76
jhsui <jiahansui1998@gmail.com>
2026-08-29 20:11:51 -0400

    remove try catch clause

--- backend/src/controllers/commentController.ts
+++ backend/src/controllers/commentController.ts
@@ -1,78 +1,70 @@
 import { commentValidator } from "../validators.ts";
 import { matchedData, validationResult } from "express-validator";
-import { type Request, type Response, type NextFunction } from "express";
+import { type Request, type Response } from "express";
 import { prisma } from "../lib/prisma.ts";
 import requireAuth from "../middleware/requireAuth.ts";
 
 const postComment = [
   ...commentValidator,
   requireAuth,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        res.status(400).json({
-          message: "Validation failed",
-          errors: errors.array(),
-        });
-        return;
-      }
-
-      const { comment } = matchedData(req);
-
-      const userId = res.locals.session.user.id;
-
-      const { postId } = req.params;
-      if (typeof postId !== "string" || postId.length === 0) {
-        return res.status(400).json({ error: "Invalid post ID" });
-      }
-
-      await prisma.comment.create({
-        data: {
-          content: comment,
-          author: {
-            connect: { id: userId },
-          },
-          post: {
-            connect: { id: postId },
-          },
-        },
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        message: "Validation failed",
+        errors: errors.array(),
       });
-
-      res.status(201).json({
-        message: "Your comment submitted successfully.",
-      });
-    } catch (error) {
-      next(error);
+      return;
     }
+
+    const { comment } = matchedData(req);
+
+    const userId = res.locals.session.user.id;
+
+    const { postId } = req.params;
+    if (typeof postId !== "string" || postId.length === 0) {
+      return res.status(400).json({ error: "Invalid post ID" });
+    }
+
+    await prisma.comment.create({
+      data: {
+        content: comment,
+        author: {
+          connect: { id: userId },
+        },
+        post: {
+          connect: { id: postId },
+        },
+      },
+    });
+
+    res.status(201).json({
+      message: "Comment submitted successfully",
+    });
   },
 ];
 
 const getComments = [
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { postId } = req.params;
-      if (typeof postId !== "string" || postId.length === 0) {
-        return res.status(400).json({ error: "Invalid post ID" });
-      }
+  async (req: Request, res: Response) => {
+    const { postId } = req.params;
+    if (typeof postId !== "string" || postId.length === 0) {
+      return res.status(400).json({ error: "Invalid post ID" });
+    }
 
-      const comments = await prisma.comment.findMany({
-        where: {
-          postId,
-        },
-        include: {
-          author: {
-            select: {
-              name: true,
-            },
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId,
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
           },
         },
-      });
+      },
+    });
 
-      res.json({ comments });
-    } catch (error) {
-      next(error);
-    }
+    res.json({ comments });
   },
 ];
 
--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -1,182 +1,148 @@
 import { writingPostValidator } from "../validators.ts";
 import { matchedData, validationResult } from "express-validator";
-import { type Request, type Response, type NextFunction } from "express";
+import { type Request, type Response } from "express";
 import { prisma } from "../lib/prisma.ts";
 import requireAuth from "../middleware/requireAuth.ts";
 
 const createPost = [
   ...writingPostValidator,
   requireAuth,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        res.status(400).json({
-          message: "Validation failed",
-          // todo: add error page
-          errors: errors.array(),
-        });
-        return;
-      }
-
-      const { title, content } = matchedData(req);
-      const userId = res.locals.session.user.id;
-
-      const post = await prisma.post.create({
-        data: {
-          title,
-          content,
-          author: {
-            connect: { id: userId },
-          },
-        },
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        message: "Validation failed",
+        // todo: add error page
+        errors: errors.array(),
       });
-
-      res.status(201).json({
-        message: "Your post submitted successfully.",
-      });
-    } catch (error) {
-      next(error);
+      return;
     }
+
+    const { title, content } = matchedData(req);
+    const userId = res.locals.session.user.id;
+
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        author: {
+          connect: { id: userId },
+        },
+      },
+    });
+
+    res.status(201).json({
+      message: "Post submitted successfully",
+    });
   },
 ];
 
 const getAllPosts = [
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const posts = await prisma.post.findMany({
-        // todo: allow to switch sort
-        orderBy: {
-          createdAt: "desc",
-        },
-      });
-      res.json({ posts });
-    } catch (error) {
-      next(error);
-    }
+  async (req: Request, res: Response) => {
+    const posts = await prisma.post.findMany({
+      // todo: allow to switch sort
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.json({ posts });
   },
 ];
 
 const getPostById = [
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { postId } = req.params;
+  async (req: Request, res: Response) => {
+    const { postId } = req.params;
 
-      if (typeof postId !== "string" || postId.length === 0) {
-        return res.status(400).json({ error: "Invalid post ID" });
-      }
+    if (typeof postId !== "string" || postId.length === 0) {
+      return res.status(400).json({ error: "Invalid post ID" });
+    }
 
-      const post = await prisma.post.findUnique({
-        where: { id: postId },
-        include: {
-          author: {
-            select: {
-              name: true,
-            },
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: {
+        author: {
+          select: {
+            name: true,
           },
         },
+      },
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Post not found",
       });
-
-      if (!post) {
-        return res.status(404).json({
-          error: "Post not found",
-        });
-      }
-
-      res.json({ post });
-    } catch (error) {
-      next(error);
     }
+
+    res.json({ post });
   },
 ];
 
 const getLikeStatus = [
   requireAuth,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { postId } = req.params;
-      const userId = res.locals.session.user.id;
+  async (req: Request, res: Response) => {
+    const { postId } = req.params;
+    const userId = res.locals.session.user.id;
 
-      if (typeof postId !== "string" || postId.length === 0) {
-        return res.status(400).json({
-          message: "postId must be a non-empty string",
-        });
-      }
-
-      const existingLike = await prisma.postLike.findUnique({
-        where: {
-          userId_postId: {
-            userId,
-            postId,
-          },
-        },
+    if (typeof postId !== "string" || postId.length === 0) {
+      return res.status(400).json({
+        message: "postId must be a non-empty string",
       });
+    }
 
-      const likeCount = await prisma.postLike.count({
-        where: {
+    const existingLike = await prisma.postLike.findUnique({
+      where: {
+        userId_postId: {
+          userId,
           postId,
         },
-      });
+      },
+    });
 
-      if (existingLike) {
-        res.json({ liked: true, likeCount });
-        return;
-      }
+    const likeCount = await prisma.postLike.count({
+      where: {
+        postId,
+      },
+    });
 
-      res.json({ liked: false, likeCount });
-    } catch (error) {
-      next(error);
+    if (existingLike) {
+      res.json({ liked: true, likeCount });
+      return;
     }
+
+    res.json({ liked: false, likeCount });
   },
 ];
 
 const togglePostLike = [
   requireAuth,
 
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { postId } = req.params;
-      const userId = res.locals.session.user.id;
+  async (req: Request, res: Response) => {
+    const { postId } = req.params;
+    const userId = res.locals.session.user.id;
 
-      if (typeof postId !== "string" || postId.length === 0) {
-        return res.status(400).json({
-          message: "postId must be a non-empty string",
-        });
-      }
+    if (typeof postId !== "string" || postId.length === 0) {
+      return res.status(400).json({
+        message: "postId must be a non-empty string",
+      });
+    }
 
-      const existingLike = await prisma.postLike.findUnique({
+    const existingLike = await prisma.postLike.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+
+    if (existingLike) {
+      await prisma.postLike.delete({
         where: {
           userId_postId: {
             userId,
             postId,
           },
-        },
-      });
-
-      if (existingLike) {
-        await prisma.postLike.delete({
-          where: {
-            userId_postId: {
-              userId,
-              postId,
-            },
-          },
-        });
-
-        const likeCount = await prisma.postLike.count({
-          where: {
-            postId,
-          },
-        });
-
-        res.json({ message: "Like cancelled.", currentLike: false, likeCount });
-        return;
-      }
-
-      await prisma.postLike.create({
-        data: {
-          userId,
-          postId,
         },
       });
 
@@ -186,10 +152,24 @@ const togglePostLike = [
         },
       });
 
-      res.json({ message: "Liked.", currentLike: true, likeCount });
-    } catch (error) {
-      next(error);
+      res.json({ message: "Like cancelled.", currentLike: false, likeCount });
+      return;
     }
+
+    await prisma.postLike.create({
+      data: {
+        userId,
+        postId,
+      },
+    });
+
+    const likeCount = await prisma.postLike.count({
+      where: {
+        postId,
+      },
+    });
+
+    res.json({ message: "Liked.", currentLike: true, likeCount });
   },
 ];
 
